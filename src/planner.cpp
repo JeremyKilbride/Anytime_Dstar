fe645f43e496edf5ed9efa7f20b4732370d2456f
Jeremy Kilbride <jkilbrid@andrew.cmu.edu>
2024-11-14 17:47:01 -0500

    added basic timing

--- src/planner.cpp
+++ src/planner.cpp
@@ -1,23 +1,34 @@
 #include "planner.h"
 #include<iostream>
+#include <chrono>
 
 using std::cout;
 
 void plannerAstar()
 {
+    std::chrono::steady_clock::time_point t_start =std::chrono::steady_clock::now();
     cout<<"using A* planner\n";
+    std::chrono::steady_clock::time_point t_end =std::chrono::steady_clock::now();
+    std::chrono::microseconds planner_time = std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start);
+    cout<<"total time: "<<(double)planner_time.count()/1000<< " ms\n";
 }
 
 void plannerDstarLite()
 {
+    std::chrono::steady_clock::time_point t_start =std::chrono::steady_clock::now();
     cout<<"using D* Lite planner\n";
-    
+    std::chrono::steady_clock::time_point t_end =std::chrono::steady_clock::now();
+    std::chrono::microseconds planner_time = std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start);
+    cout<<"total time: "<<(double)planner_time.count()/1000<< " ms\n";
 }
 
 void plannerAnytimeDstar()
 {
+    std::chrono::steady_clock::time_point t_start =std::chrono::steady_clock::now();
     cout<<"using Anytime D* planner\n";
-    
+    std::chrono::steady_clock::time_point t_end =std::chrono::steady_clock::now();
+    std::chrono::microseconds planner_time = std::chrono::duration_cast<std::chrono::microseconds>(t_end - t_start);
+    cout<<"total time: "<<(double)planner_time.count()/1000<< " ms\n";
 }
 
 enum planner{
@@ -30,8 +41,11 @@ int main(int argc, char** argv)
 {
     if (argc!=3){
         cout<<"invalid number of arguments\n";
-        cout<<"usage: ./planner [mapfile] [whichPlanner]";
+        cout<<"usage: ./planner [mapfile] [whichPlanner]\n";
+        return 0;
     }
+    std::string map_path=argv[1];
+    cout<<"got map file "<<map_path<<"\n";
     int which=std::stoi(argv[2]);
 
     switch (which)
@@ -45,6 +59,8 @@ int main(int argc, char** argv)
     case planner::ANYTIME_DSTAR:
         plannerAnytimeDstar();
         break;
+    default:
+        cout<<"planner selection not recognized, please try again with either 0, 1, or 2\n";
     }
 
     return 0;
