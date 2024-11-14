31c38a9bc4f10ee5c8da05757b27ecfa14874203
Jeremy Kilbride <jkilbrid@andrew.cmu.edu>
2024-11-14 17:35:05 -0500

    added basic framework for using different planners

--- src/planner.cpp
+++ src/planner.cpp
@@ -1,8 +1,51 @@
 #include "planner.h"
+#include<iostream>
 
+using std::cout;
 
-
-int main()
+void plannerAstar()
 {
+    cout<<"using A* planner\n";
+}
+
+void plannerDstarLite()
+{
+    cout<<"using D* Lite planner\n";
+    
+}
+
+void plannerAnytimeDstar()
+{
+    cout<<"using Anytime D* planner\n";
+    
+}
+
+enum planner{
+    ASTAR,
+    DSTAR_LITE,
+    ANYTIME_DSTAR
+};
+
+int main(int argc, char** argv)
+{
+    if (argc!=3){
+        cout<<"invalid number of arguments\n";
+        cout<<"usage: ./planner [mapfile] [whichPlanner]";
+    }
+    int which=std::stoi(argv[2]);
+
+    switch (which)
+    {
+    case planner::ASTAR:
+        plannerAstar();
+        break;
+    case planner::DSTAR_LITE:
+        plannerDstarLite();
+        break;
+    case planner::ANYTIME_DSTAR:
+        plannerAnytimeDstar();
+        break;
+    }
+
     return 0;
 }
