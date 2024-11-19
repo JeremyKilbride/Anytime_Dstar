e8109d506ca10115f8a371afed8e65c3847ccd44
EWassmann <evanjwassmann@gmail.com>
2024-11-19 14:03:24 -0500

    adding some functionality to the planner header file.

--- include/planner.h
+++ include/planner.h
@@ -4,7 +4,11 @@
 #include <unordered_map>
 #include <queue>
 #include <limits>
+#include <memory>
 
+int get_key(int x_size, int y_size, int x, int y){ //IF MAPS ARE 0 indexed
+    return y*x_size + x;
+};
 
 struct Node
 {
@@ -13,6 +17,31 @@ struct Node
     double g=std::numeric_limits<double>::max();
     double h=std::numeric_limits<double>::max();;
     double v=std::numeric_limits<double>::max();;
+    Node(int x, int y): x(x), y(y){}
+};
+
+class Graph
+{
+    private:
+        std::unordered_map<int, Node> nodeMap;
+        int x_size;
+        int y_size;
+    public:
+        Graph(int x_size, int y_size):x_size(x_size),y_size(y_size){}
+        
+        void addNode(Node newNode){
+            nodeMap.emplace(get_key(x_size,y_size,newNode.x,newNode.y),newNode);
+        }
+
+        std::shared_ptr<Node> get(Node newNode){
+            auto it = nodeMap.find(get_key(x_size,y_size,newNode.x,newNode.y));
+            if(it==nodeMap.end()){
+                return nullptr;
+            }else{
+                return std::make_shared<Node>(it->second);
+            }
+        }
+
 };
 
 #endif
